8e146f90d80697ea5ff478cc73d696e97cf01aef
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2022-11-01 21:20:43 +0200

    Sets and Dictionaries Advanced - Exercise
    Sets and Dictionaries Advanced - Exercise

---
+++ Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            SortedDictionary<char, int> charOccurrences = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char curChar = input[i];
+                if (!charOccurrences.ContainsKey(curChar))
+                {
+                    charOccurrences.Add(curChar, 0);
+                }
+                charOccurrences[curChar]++;
+            }
+
+            foreach (var character in charOccurrences)
+            {
+                Console.WriteLine($"{character.Key}: {character.Value} time/s");
+            }
+        }
+    }
+}
